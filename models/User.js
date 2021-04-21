924d2537eea5ca491cc6847bec62547a60eb646f
marvlasa <martin.vlasa@gmail.com>
2021-04-21 19:45:08 -0300

    changes DB local

--- models/User.js
+++ models/User.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  firstname: String,
-  lastname: String,
-  username: String,
+  firstName: String,
+  lastName: String,
+  userName: String,
   email: String,
-  description: String,
-  profilePicture: String,
+  password: String,
+  // description: String,
+  // profilePicture: String,
+
   //tweets: {username: String, comment: String },
   //following: {username: String},
   //followers: {username: String},
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,18 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-    res.render("home");
-})
+router.get("/", (req, res) => {
+  res.render("home");
+});
 
+router.get("/register", (req, res) => {
+  res.render("registro");
+});
+
+router.post("/register", async (req, res) => {
+  const users = new User(req.body);
+  await users.save();
+  res.render("registro");
+});
 
 module.exports = router;
