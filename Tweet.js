5a5156cf135d71a7537e8c4f00fa7697520c3ddb
trottafede <ftrotta18@gmail.com>
2021-04-21 12:03:29 -0300

    retoques

--- Tweet.js
+++ Tweet.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const Tweet = new Schema({
   text: String(140),
-  username: String,
-  createdAt: Date,
+  autor: String,
+  createdAt: { type: Date, default: Date.now },
   likes: Number,
 });
+
+// time stamp? consultar createdAt
 
 module.exports = mongoose.model("tweets", Tweet);
