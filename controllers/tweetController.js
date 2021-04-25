81fb5d5175869c565b15f83c3416b2b3dc270ce9
trottafede <ftrotta18@gmail.com>
2021-04-25 04:32:33 -0300

    tratando de mergear lo de martin

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -6,8 +6,7 @@ const router = express.Router();
 module.exports = {
   newTweet: router.post("/create", async (req, res) => {
     const { text } = req.body;
-    console.log(req.body);
-    const tweets = new Tweet({ text, author: "608289b9e6d11d3b381d2a8f" });
+    const tweets = new Tweet({ text, user: req.user });
     await tweets.save();
     res.redirect("/");
   }),
