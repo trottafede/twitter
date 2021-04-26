fc955214bfc5e52910740d87f8c0f7d28b9029d3
devsromero <80972155+devsromero@users.noreply.github.com>
2021-04-26 13:07:02 -0300

    Limpio

--- routes/tweets.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const tweetController = require("../controllers/tweetController");
-const logedIn = require("../middlewares/loginControl");
-
-router.post("/create", logedIn, tweetController.newTweet);
-router.post("/like", logedIn, tweetController.createLike);
-
-module.exports = router;
