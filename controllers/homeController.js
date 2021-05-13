66a27036a6195e406c983329f2b39880a9db8cf7
marvlasa <martin.vlasa@gmail.com>
2021-05-13 17:31:17 -0300

    modify arraydetweet

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,51 +5,12 @@ const showHome = async (req, res) => {
   // console.log("Logeado como: " + req.user);
   const user = req.user;
 
-  const tweets = await Tweet.find()
+  const arrayDeTweets = await Tweet.find()
     .sort({ createdAt: -1 })
-    .populate("user")
+    .populate("author")
     .limit(20);
-  // console.log(tweets);
-  let arrayDeTweet = [];
 
-  let count = 0;
-
-  await new Promise((resolve) => {
-    tweets.forEach(async (item) => {
-      try {
-        //some real logic
-        let newUser = await User.findById(item.author);
-
-        arrayDeTweet.push({
-          _id: item._id,
-          firstName: newUser.firstName,
-          lastName: newUser.lastName,
-          email: newUser.email,
-          image: newUser.image,
-          tweet: item.text,
-          tweetDate: item.createdAt,
-          likes: item.likes.length,
-        });
-      } catch (e) {
-        // error handling
-        // console.log(e);
-      } finally {
-        // most important is here
-        count += 1;
-        if (count == tweets.length) {
-          // console.log(arrayDeTweet);
-          resolve();
-
-          res.json({ arrayDeTweet, user });
-        }
-      }
-    });
-  });
-
-  // setTimeout(() => {
-  // console.log(arrayDeTweet);
-  //
-  // }, 10000);
+  res.json({ arrayDeTweets, user });
 };
 
 const logout = async (req, res) => {
